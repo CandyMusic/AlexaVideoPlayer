d3ca6254adae19b2b7e2d79ab71192d8ee802c36
AbhimanyuLogs <97830132+AbhimanyuLogs@users.noreply.github.com>
2022-01-23 17:44:56 +0530

    Initialize beta

---
+++ program/punishment.py
@@ -0,0 +1,166 @@
+""" global banned and un-global banned module """
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from driver.filters import command
+from driver.decorators import sudo_users_only
+from driver.database.dbchat import get_served_chats
+from driver.database.dbpunish import add_gban_user, is_gbanned_user, remove_gban_user
+
+from config import BOT_NAME, SUDO_USERS, BOT_USERNAME as bn
+
+
+@app.on_message(filters.command("gban") & filters.user(SUDOERS))
+async def ban_globally(_, message):
+    if not message.reply_to_message:
+        if len(message.command) < 2:
+            await message.reply_text("**Usage:**\n/gban [USERNAME | USER_ID]")
+            return
+        user = message.text.split(None, 2)[1]
+        if "@" in user:
+            user = user.replace("@", "")
+        user = await app.get_users(user)
+        from_user = message.from_user
+        if user.id == from_user.id:
+            return await message.reply_text(
+                "You want to gban yourself? How Fool!"
+            )
+        elif user.id == BOT_ID:
+            await message.reply_text("Should i block myself? Lmao Ded!")
+        elif user.id in SUDOERS:
+            await message.reply_text("You want to block a sudo user? KIDXZ")
+        else:
+            await add_gban_user(user.id)
+            served_chats = []
+            chats = await get_served_chats()
+            for chat in chats:
+                served_chats.append(int(chat["chat_id"]))
+            m = await message.reply_text(
+                f"**Initializing Global Ban on {user.mention}**\n\nExpected Time : {len(served_chats)}"
+            )
+            number_of_chats = 0
+            for sex in served_chats:
+                try:
+                    await app.ban_chat_member(sex, user.id)
+                    number_of_chats += 1
+                    await asyncio.sleep(1)
+                except FloodWait as e:
+                    await asyncio.sleep(int(e.x))
+                except Exception:
+                    pass
+            ban_text = f"""
+__**New Global Ban on {MUSIC_BOT_NAME}**__
+**Origin:** {message.chat.title} [`{message.chat.id}`]
+**Sudo User:** {from_user.mention}
+**Banned User:** {user.mention}
+**Banned User ID:** `{user.id}`
+**Chats:** {number_of_chats}"""
+            try:
+                await m.delete()
+            except Exception:
+                pass
+            await message.reply_text(
+                f"{ban_text}",
+                disable_web_page_preview=True,
+            )
+        return
+    from_user_id = message.from_user.id
+    from_user_mention = message.from_user.mention
+    user_id = message.reply_to_message.from_user.id
+    mention = message.reply_to_message.from_user.mention
+    sudoers = await get_sudoers()
+    if user_id == from_user_id:
+        await message.reply_text("You want to block yourself? How Fool!")
+    elif user_id == BOT_ID:
+        await message.reply_text("Should i block myself? Lmao Ded!")
+    elif user_id in sudoers:
+        await message.reply_text("You want to block a sudo user? KIDXZ")
+    else:
+        is_gbanned = await is_gbanned_user(user_id)
+        if is_gbanned:
+            await message.reply_text("Already Gbanned.")
+        else:
+            await add_gban_user(user_id)
+            served_chats = []
+            chats = await get_served_chats()
+            for chat in chats:
+                served_chats.append(int(chat["chat_id"]))
+            m = await message.reply_text(
+                f"**Initializing Gobal Ban on {mention}**\n\nExpected Time : {len(served_chats)}"
+            )
+            number_of_chats = 0
+            for sex in served_chats:
+                try:
+                    await app.ban_chat_member(sex, user_id)
+                    number_of_chats += 1
+                    await asyncio.sleep(1)
+                except FloodWait as e:
+                    await asyncio.sleep(int(e.x))
+                except Exception:
+                    pass
+            ban_text = f"""
+__**New Global Ban on {MUSIC_BOT_NAME}**__
+**Origin:** {message.chat.title} [`{message.chat.id}`]
+**Sudo User:** {from_user_mention}
+**Banned User:** {mention}
+**Banned User ID:** `{user_id}`
+**Chats:** {number_of_chats}"""
+            try:
+                await m.delete()
+            except Exception:
+                pass
+            await message.reply_text(
+                f"{ban_text}",
+                disable_web_page_preview=True,
+            )
+            return
+
+
+@app.on_message(filters.command("ungban") & filters.user(SUDOERS))
+async def unban_globally(_, message):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            await message.reply_text(
+                "**Usage:**\n/ungban [USERNAME | USER_ID]"
+            )
+            return
+        user = message.text.split(None, 1)[1]
+        if "@" in user:
+            user = user.replace("@", "")
+        user = await app.get_users(user)
+        from_user = message.from_user
+        sudoers = await get_sudoers()
+        if user.id == from_user.id:
+            await message.reply_text("You want to unblock yourself?")
+        elif user.id == BOT_ID:
+            await message.reply_text("Should i unblock myself?")
+        elif user.id in sudoers:
+            await message.reply_text("Sudo users can't be blocked/unblocked.")
+        else:
+            is_gbanned = await is_gbanned_user(user.id)
+            if not is_gbanned:
+                await message.reply_text("He's already free, why bully him?")
+            else:
+                await remove_gban_user(user.id)
+                await message.reply_text(f"Ungbanned!")
+        return
+    from_user_id = message.from_user.id
+    user_id = message.reply_to_message.from_user.id
+    mention = message.reply_to_message.from_user.mention
+    sudoers = await get_sudoers()
+    if user_id == from_user_id:
+        await message.reply_text("You want to unblock yourself?")
+    elif user_id == BOT_ID:
+        await message.reply_text(
+            "Should i unblock myself? But i'm not blocked."
+        )
+    elif user_id in sudoers:
+        await message.reply_text("Sudo users can't be blocked/unblocked.")
+    else:
+        is_gbanned = await is_gbanned_user(user_id)
+        if not is_gbanned:
+            await message.reply_text("He's already free, why bully him?")
+        else:
+            await remove_gban_user(user_id)
+            await message.reply_text(f"Ungbanned!")
